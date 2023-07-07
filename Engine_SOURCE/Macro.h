afd59f06ab13d01db868da6c9243b06c8232c870
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-07-08 02:43:48 +0900

    singleton관련 매크로 추가

--- Engine_SOURCE/Macro.h
+++ Engine_SOURCE/Macro.h
@@ -1,8 +1,7 @@
 #pragma once
 
 
-
 #define DT TimeMgr::DeltaTime()
 
-#define KEY_TAP(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::TAP
-#define KEY_DOWN(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::DOWN
+#define KEY_TAP(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::TAP)
+#define KEY_DOWN(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::DOWN)
