fbb766a18f37ce0de83b68186b431305c2c21b34
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-07-07 19:46:56 +0900

    피직스 이식 완료 및 기존 프로젝트 정상 작동 확인)

--- Engine_SOURCE/Engine.h
+++ Engine_SOURCE/Engine.h
@@ -21,11 +21,36 @@
 #include <thread>
 #include <future>
 
+//#include <PxPhysicsAPI.h>
+#include "../External/PhysX/Include/PxPhysicsAPI.h"
+
+
+#ifdef _DEBUG
+
+
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysX_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysXFoundation_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysXCommon_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysXExtensions_static_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysXPvdSDK_static_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/debug/PhysXCharacterKinematic_static_64.lib")
+#else
+
+
+#pragma comment(lib, "../External/PhysX/lib/release/PhysX_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/release/PhysXFoundation_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/release/PhysXCommon_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/release/PhysXExtensions_static_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/release/PhysXPvdSDK_static_64.lib")
+#pragma comment(lib, "../External/PhysX/lib/release/PhysXCharacterKinematic_static_64.lib")
+#endif
+
 // ImGui_internal.h
 // ImGui.h 보다 먼저 선언 되어야 하므로 미리 선언 해둠
 #ifndef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
 #endif
+
 
 #include "CommonInclude.h"
 #include "druEnums.h"
