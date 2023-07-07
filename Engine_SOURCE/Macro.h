13c4ee0c85b2d44305ac99cb5f488d0fd7eab04a
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-07-08 02:35:10 +0900

    singleton 관련 매크로 추가

--- Engine_SOURCE/Macro.h
+++ Engine_SOURCE/Macro.h
@@ -1,8 +1,44 @@
 #pragma once
 
-
-
 #define DT TimeMgr::DeltaTime()
 
 #define KEY_TAP(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::TAP)
 #define KEY_DOWN(key) (Input::GetKeyState(eKeyCode::key) == eKeyState::DOWN)
+
+#define SINGLE(type)						\
+public:										\
+	static type* GetInstance()				\
+	{										\
+		static type* mInstance;				\
+		if (mInstance == nullptr)			\
+		{									\
+			mInstance = new type();			\
+			return mInstance;				\
+		}									\
+		return mInstance;					\
+	}										\
+											\
+	static void DestroyInstance()			\
+	{										\
+	if (GetInstance())						\
+		delete GetInstance();				\
+	}										\
+											\
+private:									\
+	type();									\
+	~type();								\
+
+#define GETSINGLE(type)						\
+type::GetInstance()							\
+
+// 반환형, 타겟, 생성되는 메소드명
+#define GETSET(__TYPE__, __TARGET__, __METHODNAME__) \
+__TYPE__ Get##__METHODNAME__()	const				 \
+{													 \
+	return __TARGET__;								 \
+}													 \
+													 \
+void Set##__METHODNAME__(__TYPE__ value)			 \
+{													 \
+	__TARGET__ = value;								 \
+}	
