40506f5c7f08a2d7e9af62100e0bfb633a50d3a1
shroow <shroows@gmail.com>
2023-08-21 15:53:23 +0900

    에디터 구조 정리

--- Dx3dEngine/guiEditorObjMgr.h
+++ Dx3dEngine/guiEditorObjMgr.h
@@ -1,9 +1,31 @@
 #pragma once
+#include "Engine.h"
+#include "EngineResource.h"
+#include "SimpleMath.h"
+#include "Graphics.h"
+#include "guiEditorObject.h"
+#include "guiDebugObject.h"
 
 namespace gui
 {
 	class EditorObjMgr
 	{
+		SINGLE(EditorObjMgr)
+
+	public:
+		void Initialize();
+		void Release();
+
+		void Run();
+
+		void Update();
+		void FixedUpdate();
+		void Render();
+		void DebugRender(DebugMesh& mesh);
+		
+	private:
+		std::vector<EditorObject*> mEditorObjects;
+		std::vector<DebugObject*> mDebugObjects;
 	};
 }
 
