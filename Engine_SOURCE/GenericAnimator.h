fba94236dba150fe21b592b6ee81beff0463e5fb
Limsunwoo1 <tjsdnhj@gmail.com>
2023-09-23 01:16:06 +0900

    GenericAnimator 주석추가 및 코드정리

--- Engine_SOURCE/GenericAnimator.h
+++ Engine_SOURCE/GenericAnimator.h
@@ -2,22 +2,18 @@
 #include "Component.h"
 #include <functional>
 
-// Invalid 값이라는거는 작동하지 않는 무의미한 상태를 나타내는 아이디
-const   int InvalidHandleId = 0;
-static   int HandleId = 0;
-
 // 매 틱 혹은 애니메이션이 끝났을때 호출하는 함수 타입
 typedef std::function<void(float)> FAnimEvent;
 
 // 애니메이션이 어떻게 변화하는지
-enum class eAnimType {
+enum class eAnimType
+{
     Linear,            // 일단 단순하게 선형보간 천천히 균일하게 증가
     Max,
 };
 
 typedef struct _AnimatorParam
 {
-    int Handle;
     eAnimType AnimType;
 
     float StartValue;
@@ -28,7 +24,7 @@ typedef struct _AnimatorParam
     FAnimEvent DurationFunc;
     FAnimEvent CompleteFunc;
 
-    _AnimatorParam() : Handle(InvalidHandleId), AnimType(eAnimType::Linear), StartValue(0.f), EndValue(1.f), DurationTime(1.f), DurationFunc(nullptr), CompleteFunc(nullptr) {}
+    _AnimatorParam() : AnimType(eAnimType::Linear), StartValue(0.f), EndValue(1.f), DurationTime(1.f), DurationFunc(nullptr), CompleteFunc(nullptr) {}
 
 }AnimatorParam;
 
@@ -38,17 +34,22 @@ public:
     GenericAnimator();
     ~GenericAnimator();
 
+    // 진행될 파라미터
     bool Start(const AnimatorParam& InParam);
-    void Update(float InDeltaTime);
 
+    // 현재 진행중인지 여부
     bool IsRunning();
+
+    // true 이면 CompleateFunc 호출 
     void Stop(bool InCompleteEvent = false);
 
-    virtual void Initialize() override;
+    virtual void Initialize() override {};
     virtual void Update() override;
-    virtual void FixedUpdate() override;
-    virtual void Render() override;
+    virtual void FixedUpdate() override{};
+    virtual void Render() override{};
 
+private:
+    void update(float InDeltaTime);
 private:
     AnimatorParam Param;
 
@@ -58,6 +59,8 @@ private:
 
     float AccTime;
     float EndTime;
+
+    bool mbRunning;
 };
 
 
@@ -76,6 +79,18 @@ private:
       param.EndValue = 200.f;
       param.DurationTime = 3.f;
       //param.DurationFunc = [](float InCurValue) {};
+      
+      ==================================================
+      람다 함수 내부에서는 중단점이 걸리지 않기때문에
+      복잡한 로직같은경우 클래스 내부 함수를 해당 람다함수에서
+      호출하는식으로 구현하는것을 권장합니다.
+
+      ex ) param.DurationFunc = [this](float InCurValue)
+            {
+                TestFunc(InCurValue);
+            };
+      ===================================================
+
       param.DurationFunc = [this](float InCurValue)
       {
          LOG(STRING("진행 : %f", InCurValue));
