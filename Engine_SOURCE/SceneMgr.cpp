3a600c287daee48c4adbf2ea56146b13f83d4177
durumyisking <80432748+Durumyisking@users.noreply.github.com>
2023-10-24 19:57:27 +0900

    절두체 컬링하는 로직 함수화 디렉셔널라이트 두 개 추가되는 버그 제거 정확한 프레임 체크 위해 출력용 DT를 만듬

--- Engine_SOURCE/SceneMgr.cpp
+++ Engine_SOURCE/SceneMgr.cpp
@@ -22,6 +22,8 @@
 #include "ModelObj.h"
 #include "SkySphere.h"
 
+#include "BlockBrick.h"
+
 
 SceneMgr::SceneMgr()
 	: mScenes{}
@@ -253,6 +255,9 @@ void SceneMgr::CreateCDO()
 
 	ModelObj* ModelObjCDO = new ModelObj();
 	GameObj::AddObjectCDO("ModelObj", ModelObjCDO);
+
+	BlockBrick* BlockBrickObjCDO = new BlockBrick();
+	GameObj::AddObjectCDO("BlockBrick", BlockBrickObjCDO);
 
 	//SkySphere* SkySphere = new SkySphere();
 	//GameObj::AddObjectCDO("SkySphere", SkySphere);
