a1a68a0da2878178ed7dae11f4e4e3b6528ccf96
motical <motical9833@gmail.com>
2023-10-25 17:44:07 +0900

    MapObject Class
    MapObject Class 작성 초기

---
+++ Engine_SOURCE/MapObject.cpp
@@ -0,0 +1,108 @@
+#include "MapObject.h"
+#include "Physical.h"
+#include "Model.h"
+#include "MeshRenderer.h"
+#include "PhysXRigidBody.h"
+#include "PhysXCollider.h"
+
+MapObject::MapObject(std::wstring modelName, Vector3 physicalScale)
+	: GameObj()
+	, mModelName(modelName)
+	, mbPhysical(true)
+	, mPhysicalScale(physicalScale)
+	, mGeometryType(eGeometryType::Box)
+	, mActorType(eActorType::Static)
+{
+	assert(AddComponent<MeshRenderer>(eComponentType::MeshRenderer));
+	mObjectTypeName = "MapObject";
+}
+
+MapObject::MapObject(const MapObject& Obj)
+	:GameObj(Obj)
+	, mModelName(Obj.mModelName)
+	, mbPhysical(Obj.mbPhysical)
+	, mPhysicalScale(Obj.mPhysicalScale)
+	, mGeometryType(Obj.mGeometryType)
+	, mActorType(Obj.mActorType)
+{
+	assert(AddComponent<MeshRenderer>(eComponentType::MeshRenderer));
+}
+
+MapObject::~MapObject()
+{
+}
+
+MapObject* MapObject::Clone() const
+{
+	return new MapObject(*this);
+}
+
+void MapObject::Initialize()
+{
+	Model* model = GETSINGLE(ResourceMgr)->Find<Model>(mModelName);
+	assert(model);
+
+	MeshRenderer* mr = GetComponent<MeshRenderer>();
+	mr->SetMaterial(model->GetMaterial());
+	mr->SetModel(model);
+
+	this->GetComponent<Transform>()->SetOffsetScale(0.005f);
+
+	if (mbPhysical)
+	{
+		if (GetComponent<Physical>() != nullptr)
+		{
+			Physical* physical = GetComponent<Physical>();
+			delete physical;
+			physical = nullptr;
+
+			PhysXRigidBody* rigid = GetComponent<PhysXRigidBody>();
+			delete rigid;
+			rigid = nullptr;
+
+			PhysXCollider* collider = GetComponent<PhysXCollider>();
+			delete collider;
+			collider = nullptr;
+		}
+		Physical* physical = AddComponent<Physical>(eComponentType::Physical);
+		physical->InitialDefaultProperties(mActorType, mGeometryType, mPhysicalScale);
+
+		PhysXRigidBody* rigid = AddComponent<PhysXRigidBody>(eComponentType::RigidBody);
+		rigid->RemoveGravity();
+
+		AddComponent<PhysXCollider>(eComponentType::Collider);
+	}
+
+	GameObj::Initialize();
+}
+
+void MapObject::Update()
+{
+	GameObj::Update();
+}
+
+void MapObject::FixedUpdate()
+{
+	GameObj::FixedUpdate();
+}
+
+void MapObject::Render()
+{
+	GameObj::Render();
+}
+
+void MapObject::OnCollisionEnter(GameObj* gameObject)
+{
+}
+
+void MapObject::OnTriggerEnter(GameObj* gameObject)
+{
+}
+
+void MapObject::OnTriggerPersist(GameObj* gameObject)
+{
+}
+
+void MapObject::OnTriggerExit(GameObj* gameObject)
+{
+}
---
+++ Engine_SOURCE/MapObject.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "GameObj.h"
+
+class MeshRenderer;
+class MapObject : public GameObj
+{
+public:
+	MapObject(std::wstring mModelName, Vector3 mPhysicalScale;);
+	MapObject(const MapObject& Obj);
+	virtual ~MapObject();
+
+
+	virtual MapObject* Clone() const;
+	virtual void Initialize() override;
+	virtual void Update() override;
+	virtual void FixedUpdate() override;
+	virtual void Render() override;
+
+public:
+	virtual void OnCollisionEnter(GameObj* gameObject) override;
+	virtual void OnTriggerEnter(GameObj* gameObject) override;
+	virtual void OnTriggerPersist(GameObj* gameObject) override;
+	virtual void OnTriggerExit(GameObj* gameObject) override;
+
+private:
+	std::wstring mModelName;
+	bool mbPhysical;
+	Vector3 mPhysicalScale;
+	eGeometryType mGeometryType;
+	eActorType mActorType;
+};
